ac3a057a2e8fef79c1ed6cdb945bc89d76c7887c
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-25 15:05:52 +0000

    git-svn-id: https://plugins.svn.wordpress.org/graphical-admin-report/tags/1.0@267387 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ extra.php
@@ -0,0 +1,21 @@
+<?php
+function timepass() 
+{
+	?>
+	<div align="center" style="padding:10px;">
+	<script type="text/javascript"><!--
+	google_ad_client = "pub-6676019656533652";
+	/* 728x90, created 8/20/09 */
+	google_ad_slot = "7129763702";
+	google_ad_width = 728;
+	google_ad_height = 90;
+	//-->
+	</script>
+	<script type="text/javascript"
+	src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+	</script>
+	</div>
+    <?php
+	return getadd;
+}
+?>
