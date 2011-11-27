442fbf29c2c2aa47bff9230047c5e87d8707c31a
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-27 02:52:56 +0000

    git-svn-id: https://plugins.svn.wordpress.org/graphical-admin-report/trunk@468071 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ graphical-admin-report.php
@@ -0,0 +1,333 @@
+<?php
+
+/*
+Plugin Name: Graphical admin report
+Plugin URI: http://www.gopiplus.com/work/2010/07/18/graphical-admin-report/
+Description: This plug-in will display the graphical report for admin about post count, user registration, comments posted activity.
+Version: 4.0
+Author: Gopi.R
+Author URI: http://www.gopiplus.com/work/2010/07/18/graphical-admin-report/
+*/
+
+/**
+ *     Graphical admin report
+ *     Copyright (C) 2011  www.gopiplus.com
+ * 
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+$Greport_ColorCounter=0;
+$Greport_arr_Colors[0] = "0099CC" ;
+$Greport_arr_Colors[1] = "FF0000" ;
+$Greport_arr_Colors[2] = "8BBA00";
+$Greport_arr_Colors[3] = "F6BD0F";
+$Greport_arr_Colors[4] = "A66EDD";
+$Greport_arr_Colors[5] = "F984A1" ;
+$Greport_arr_Colors[6] = "CCCC00" ;
+$Greport_arr_Colors[7] = "999999" ;
+$Greport_arr_Colors[8] = "1941A5" ;
+$Greport_arr_Colors[9] = "AFD8F8";
+$Greport_arr_Colors[10] = "006F00" ;
+$Greport_arr_Colors[11] = "0099FF"; 
+$Greport_arr_Colors[12] = "FF66CC" ;
+$Greport_arr_Colors[13] = "669966" ;
+$Greport_arr_Colors[14] = "7C7CB4" ;
+$Greport_arr_Colors[15] = "FF9933" ;
+$Greport_arr_Colors[16] = "9900FF" ;
+$Greport_arr_Colors[17] = "99FFCC" ;
+$Greport_arr_Colors[18] = "CCCCFF" ;
+$Greport_arr_Colors[19] = "669900" ;
+
+function getFCColor() 
+{
+	global $Greport_ColorCounter;
+	global $Greport_arr_Colors;
+	$Greport_ColorCounter++;
+	return($Greport_arr_Colors[$Greport_ColorCounter % count($Greport_arr_Colors)]);
+}
+
+function Greport_deactivate() 
+{
+
+}
+
+function Greport_activation() 
+{
+
+}
+
+function Greport_admin_options() 
+{
+	global $wpdb;
+	$Choosen_date_range_graph = "";
+	
+	$siteurl = get_option('siteurl');
+	$pluginurl = "/wp-content/plugins/graphical-admin-report";
+	$fullpluginurl = $siteurl.$pluginurl;
+	
+	$mainurl = $siteurl."/wp-admin/options-general.php?page=graphical-admin-report/graphical-admin-report.php";
+	
+	?>
+<SCRIPT LANGUAGE="Javascript" SRC="<?php echo $fullpluginurl; ?>/FusionCharts.js"></SCRIPT>
+<?php
+	include("graphical-admin-chart.php");
+	?>
+
+<div class="wrap">
+  <h2>Graphical Report</h2>
+</div>
+<?php
+	
+	$f_month = @$_POST['f_month'];
+	$f_day	= @$_POST['f_day'];
+	$f_year	= @$_POST['f_year'];
+	$t_month = @$_POST['t_month'];
+	$t_day	= @$_POST['t_day'];
+	$t_year	= @$_POST['t_year'];
+	$report_type = @$_POST['report_type'];
+	$record_type = @$_POST['record_type'];
+
+	
+	?>
+<form name="greportfrm" id="greportfrm" method="post" action="<?php echo $mainurl; ?>">
+  <table width="100%" border="0" cellspacing="0" cellpadding="2">
+    <tr>
+      <td width="452">Choose date range</td>
+      <td width="111">Choose  type</td>
+      <td width="156">Choose graph type</td>
+      <td width="65">&nbsp;</td>
+    </tr>
+    <tr>
+      <td><select name="f_month" id="f_month">
+          <option value="">Month</option>
+          <option <?php if($f_month=="1") { echo "selected=''"; } ?> value="1">Jan</option>
+          <option <?php if($f_month=="2") { echo "selected=''"; } ?> value="2">Feb</option>
+          <option <?php if($f_month=="3") { echo "selected=''"; } ?>  value="3">Mar</option>
+          <option <?php if($f_month=="4") { echo "selected=''"; } ?> value="4">Apr</option>
+          <option <?php if($f_month=="5") { echo "selected=''"; } ?> value="5">May</option>
+          <option <?php if($f_month=="6") { echo "selected=''"; } ?> value="6">Jun</option>
+          <option <?php if($f_month=="7") { echo "selected=''"; } ?> value="7">Jul</option>
+          <option <?php if($f_month=="8") { echo "selected=''"; } ?> value="8">Aug</option>
+          <option <?php if($f_month=="9") { echo "selected=''"; } ?> value="9">Sep</option>
+          <option <?php if($f_month=="10") { echo "selected=''"; } ?> value="10">Oct</option>
+          <option <?php if($f_month=="11") { echo "selected=''"; } ?> value="11">Nov</option>
+          <option <?php if($f_month=="12") { echo "selected=''"; } ?> value="12">Dec</option>
+        </select>
+        <select name="f_day" id="f_day">
+          <option value="" >Day</option>
+          <?php
+		  
+        for($i=1;$i<=31;$i++)
+        {
+		echo "==";
+        ?>
+          <option <?php if($i==$f_day) { echo "selected=''"; } ?> value="<?php echo $i; ?>" ><?php echo $i; ?></option>
+          <?php
+        }
+        ?>
+        </select>
+        <select name="f_year" id="f_year">
+          <option value="" >Year</option>
+          <?php
+        for($i=2005;$i<=2015;$i++)
+        {
+        ?>
+          <option <?php if($i==$f_year) { echo "selected=''"; } ?> value="<?php echo $i; ?>"><?php echo $i; ?></option>
+          <?php
+        }
+        ?>
+        </select>
+        -
+        <select name="t_month" id="t_month">
+          <option value="">Month</option>
+          <option <?php if($t_month=="1") { echo "selected=''"; } ?> value="1">Jan</option>
+          <option <?php if($t_month=="2") { echo "selected=''"; } ?> value="2">Feb</option>
+          <option <?php if($t_month=="3") { echo "selected=''"; } ?> value="3">Mar</option>
+          <option <?php if($t_month=="4") { echo "selected=''"; } ?> value="4">Apr</option>
+          <option <?php if($t_month=="5") { echo "selected=''"; } ?> value="5">May</option>
+          <option <?php if($t_month=="6") { echo "selected=''"; } ?> value="6">Jun</option>
+          <option <?php if($t_month=="7") { echo "selected=''"; } ?> value="7">Jul</option>
+          <option <?php if($t_month=="8") { echo "selected=''"; } ?> value="8">Aug</option>
+          <option <?php if($t_month=="9") { echo "selected=''"; } ?> value="9">Sep</option>
+          <option <?php if($t_month=="10") { echo "selected=''"; } ?> value="10">Oct</option>
+          <option <?php if($t_month=="11") { echo "selected=''"; } ?> value="11">Nov</option>
+          <option <?php if($t_month=="12") { echo "selected=''"; } ?> value="12">Dec</option>
+        </select>
+        <select name="t_day" id="t_day">
+          <option value="" >Day</option>
+          <?php
+        for($i=1;$i<=31;$i++)
+        {
+        ?>
+          <option <?php if($i==$t_day) { echo "selected=''"; } ?> value="<?php echo $i; ?>" ><?php echo $i; ?></option>
+          <?php
+        }
+        ?>
+        </select>
+        <select name="t_year" id="t_year">
+          <option value="" >Year</option>
+          <?php
+        for($i=2005;$i<=2015;$i++)
+        {
+        ?>
+          <option <?php if($i==$t_year) { echo "selected=''"; } ?> value="<?php echo $i; ?>"><?php echo $i; ?></option>
+          <?php
+        }
+        ?>
+        </select>
+      </td>
+      <td><select name="report_type" id="report_type">
+          <option <?php if($report_type=="Month") { echo "selected=''"; } ?> value="Month">Month</option>
+          <option <?php if($report_type=="Day") { echo "selected=''"; } ?> value="Day">Day</option>
+        </select></td>
+      <td><select name="record_type" id="report_type">
+          <option <?php if($record_type=="Posts report graph") { echo "selected=''"; } ?> value="Posts report graph">Posts report</option>
+          <option <?php if($record_type=="User registration graph") { echo "selected=''"; } ?> value="User registration graph">User registration</option>
+          <option <?php if($record_type=="Comments report graph") { echo "selected=''"; } ?> value="Comments report graph">Comments report</option>
+        </select>
+      </td>
+      <td><input name="Greport_search" class="button-primary" id="Greport_search" type="submit" value="Submit" /></td>
+    </tr>
+  </table>
+</form>
+<?php
+	if($record_type == "User registration graph")
+	{
+		$table_string = "$wpdb->users";
+		$date_string = "user_registered";
+		$where_string = "where 1=1 ";
+	}
+	elseif($record_type == "Comments report graph")
+	{
+		$table_string = "$wpdb->comments";
+		$date_string = "comment_date";
+		$where_string = "where comment_type<>'pingback'";
+	}
+	else
+	{
+		$table_string = "$wpdb->posts";
+		$date_string = "post_date";
+		$where_string = "where post_type = 'post'";
+		$record_type = "Posts report graph";
+	}
+	if($report_type == "")
+	{
+		$report_type = "Month";
+	}
+	if($f_month <> "" && $f_day <> "" && $f_year <> "" && $t_month <> "" && $t_day <> "" && $t_year <> "")
+	{
+		$Choosen_date_range = $f_month ."/". $f_day ."/". $f_year . " - " . $t_month ."/". $t_day ."/". $t_year ;
+		$Choosen_date_range_graph = "(" . $f_month ."/". $f_day ."/". $f_year . "  -  " . $t_month ."/". $t_day ."/". $t_year . ")";
+	}
+	else
+	{
+		$Choosen_date_range = "Not selected (or) Not selected properly";
+	}
+	echo "<i><br>Choosen date range   : " . $Choosen_date_range;
+	echo "<br>Choosen type   : " . $report_type ;
+	echo "<br>Choosen graph type   : " . $record_type ;
+	echo "<br><br></i>";
+	$sSql =	"SELECT";
+	if($report_type == "Day") {
+	$sSql =	$sSql . " DAY($date_string) as d,"; 
+	}
+	$sSql =	$sSql . " MONTH($date_string) as m, YEAR($date_string) as y, COUNT(*) as tot";
+	$sSql =	$sSql . " FROM $table_string $where_string";
+	if($f_month <> "" && $f_day <> "" && $f_year <> "" && $t_month <> "" && $t_day <> "" && $t_year <> "") {
+		$sSql =	$sSql . " and $date_string BETWEEN '$f_year-$f_month-$f_day' AND '$t_year-$t_month-$t_day'"; 
+	}
+	$sSql =	$sSql . " GROUP BY";
+	if($report_type == "Day") {
+	$sSql =	$sSql . " DAY($date_string), ";
+	}
+	$sSql =	$sSql . " MONTH($date_string), YEAR($date_string)";
+	$sSql =	$sSql . " order by";
+	$sSql =	$sSql . " YEAR($date_string) desc,MONTH($date_string) desc";
+	if($report_type == "Day") {
+	$sSql =	$sSql . " ,DAY($date_string) desc"; 
+	}
+
+
+
+	//------------------------------Monthly post Summary-----------------------------------------------------
+	//$sSql =	"SELECT MONTH(post_date) as m, YEAR(post_date) as y, COUNT(*) as tot";
+	//$sSql =	$sSql . " FROM $wpdb->posts where post_type = 'post'";
+	//$sSql =	$sSql . " GROUP BY MONTH(post_date), YEAR(post_date) order by YEAR(post_date) desc,MONTH(post_date) desc limit 0,12";
+	
+	//echo "<br><br><br><br>" . $sSql;
+	$data = $wpdb->get_results($sSql);
+	$i = 0;
+	$graph_ststus=0;
+	$arrposts = array();
+	$monthnames = array(1 => 'January',2 => 'February',3 => 'March',4 => 'April',5 => 'May',6 => 'June',7 => 'July',8 => 'August',9 => 'September',10 => 'October',11 => 'November',12 => 'December');
+    foreach ( $data as $data ) 
+	{ 
+		$arrposts[$i][1] = $monthnames[$data->m];
+		$arrposts[$i][2] = $data->tot;
+		$arrposts[$i][3] = $data->y;
+		if($report_type == "Day") {
+			$arrposts[$i][4] = $data->d;
+		}
+		$i = $i+1; 
+	} 
+	if($i > 0) { $graph_ststus = 1; }
+	$strXML = "<graph caption='$report_type - $Choosen_date_range_graph  $record_type' subcaption='' xAxisName='$report_type' yAxisMinValue='0' yAxisName='Total' decimalPrecision='0' formatNumberScale='0' numberPrefix=' ' showNames='1' showValues='0' showAlternateHGridColor='1' canvasBgColor='E1EEF4' canvasBaseColor='CCE1EC' hovercapbgColor='FFECAA'  hovercapborder='F47E00' baseFontColor='1A5873' lineColor='2EA0D1' divLineColor='8cbdd5' divLineAlpha='20' alternateHGridAlpha='5' rotateNames='1'>";
+	foreach ($arrposts as $arSubData)
+	{
+		if($report_type == "Day") 
+		{ 
+			$strXML = $strXML . "<set color='". getFCColor() ."' name='" . $arSubData[4] . "th " . $arSubData[1] . " " . $arSubData[3] . "' value='" . $arSubData[2] ."' hoverText='" . $arSubData[4] . "th " . $arSubData[1] . " " . $arSubData[3] . "' />";
+		}
+		else
+		{
+			$strXML = $strXML . "<set color='". getFCColor() ."' name='" . $arSubData[1] . " " . $arSubData[3] . "' value='" . $arSubData[2] ."' hoverText='" . $arSubData[1] . "' />";
+		}
+	}
+	$strXML = $strXML . "</graph>";
+	if($graph_ststus==1)
+	{
+		echo renderChart("$fullpluginurl/FCF_Column3D.swf", "", $strXML, "wp_posts", 800, 450, false, false);
+	}
+	else
+	{
+		echo "<div align='center'>At present monthly post summary graph not available.</div>";
+	}
+	//---------------------------------------------------------------------------------------------------------
+
+
+	//----------------------------------------------------------------------------------------------------------
+	?>
+	<h2>About plugin!</h2>
+	Plug-in created by <a target="_blank" href='http://www.gopiplus.com/work/2010/07/18/graphical-admin-report/'>www.gopiplus.com</a><br>
+	Check official website for Live demo and More information <a target="_blank" href='http://www.gopiplus.com/work/2010/07/18/graphical-admin-report/'>Click here</a><br>
+<?php
+	
+}
+
+function Greport_add_to_menu() 
+{
+	add_options_page('Graphical Report', 'Graphical Report', 'manage_options', __FILE__, 'Greport_admin_options' );
+	
+}
+
+if (is_admin()) 
+{
+	add_action('admin_menu', 'Greport_add_to_menu');
+}
+
+register_activation_hook(__FILE__, 'Greport_activation');
+register_deactivation_hook( __FILE__, 'Greport_deactivate' );
+
+?>
---
+++ readme.txt
@@ -0,0 +1,111 @@
+=== Graphical admin report ===
+Contributors: Gopi.R 
+Donate link: http://www.gopiplus.com/work/2010/07/18/graphical-admin-report/
+Author URI: http://www.gopiplus.com/work/2010/07/18/graphical-admin-report/
+Plugin URI: http://www.gopiplus.com/work/2010/07/18/graphical-admin-report/
+Tags: graphical, statistics, report, graph, fusion chart,fusion graph, analysis, admin,
+Requires at least: 2.8
+Tested up to: 3.3
+Stable tag: 4.0
+	
+This plug-in will display the graphical report for admin about post count, user registration, comments posted activity.
+
+== Description ==
+
+Check official website for Live demo [http://www.gopiplus.com/work/2010/07/18/graphical-admin-report/](http://www.gopiplus.com/work/2010/07/18/graphical-admin-report/)
+
+*   [Live demo](http://www.gopiplus.com/work/2010/07/18/graphical-admin-report/)	
+*   [Plugin Site](http://www.gopiplus.com/work/)	
+*   [More info](http://www.gopiplus.com/work/2010/07/18/graphical-admin-report/)			
+*   [Comments/Suggestion](http://www.gopiplus.com/work/2010/07/18/graphical-admin-report/)		
+
+Plug Graphical admin report will display the statistics of your wordpress site on the criteria for registration of users, number of published records and comments of users. The administrator can monitor in graphical form on the intervals - months, days.
+
+To install the Graphical admin report is sufficient to activate the plugin in the administrative panel of wordpress.
+
+   1. User registration count graphical report,
+   2. Post count graphical report,
+   3. Comments posted count graphical report
+   4. Search option available to search date range.
+   5. search option available to see the grapth based om month or date.
+	
+This is my secong graphical report plugin, to see my another "graphical statistics report" clcik below URL
+
+[graphical statistics report](http://www.gopiplus.com/work/2010/07/18/graphical-admin-report/)	
+
+[Live demo](http://www.gopiplus.com/work/2010/07/18/graphical-admin-report/)	
+[Plugin Site](http://www.gopiplus.com/work/)	
+[More info](http://www.gopiplus.com/work/2010/07/18/graphical-admin-report/)			
+[Comments/Suggestion](http://www.gopiplus.com/work/2010/07/18/graphical-admin-report/)		
+[About author](http://www.gopiplus.com/)		
+
+
+To see the report:
+
+1. Activate the plugin.
+2. Go to 'Graphical Report' link under SETTINGS TAB to see the report graph.  
+
+Thanks to fusion Charts team for this SWF graph.
+
+== Installation ==
+
+**Installation Instruction & Configuration**  
+
+*   Unpack the *.zip file and extract the /graphical-admin-report/ folder.    
+*   Drop the 'graphical-statistics-report' folder into your 'wp-content/plugins' folder    
+*   In word press administration panels, click on plug-in from the menu.    
+*   You should see your new 'Graphical admin report' plug-in listed under Inactive plug-in tab.    
+*   To turn the word presses plug-in on, click activate.    
+*   Go to Graphical statistics link under settings to see the report graph.     
+
+[More info](http://www.gopiplus.com/work/2010/07/18/graphical-admin-report/)	
+
+== Frequently Asked Questions ==
+
+[More info](http://www.gopiplus.com/work/2010/07/18/graphical-admin-report/)	
+
+
+**How to display this plug-in to front end?**  
+No, this plugin only for admin report.  
+  
+
+== Screenshots ==
+
+[click to see screen](hhttp://www.gopiplus.com/work/2010/07/18/graphical-admin-report/)
+
+1. Graphical report.
+
+== Upgrade Notice == 
+
+= 4.0 =
+Tested upto 3.3
+
+= 3.0 =
+Tested upto 3.2.1
+
+= 2.0 =		
+Tested upto 3.0.
+
+= 1.0 =		
+First version.
+
+== Changelog ==
+
+[More info](http://www.gopiplus.com/work/2010/07/18/graphical-admin-report/)
+
+*4.0*
+Tested upto 3.3
+
+
+*3.0*
+Tested upto 3.2.1
+
+*2.0*	
+
+Tested upto 3.0.
+
+*1.0*
+	
+First version.
+
+	
